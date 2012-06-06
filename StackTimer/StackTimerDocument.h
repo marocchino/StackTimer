25bfccefa175b1043160f858289aacc98aff02c9
tw shim <marocchino@gmail.com>
2012-06-07 02:00:46 +0900

    some test codes

--- StackTimer/StackTimerDocument.h
+++ StackTimer/StackTimerDocument.h
@@ -8,6 +8,23 @@
 
 #import <Cocoa/Cocoa.h>
 
-@interface StackTimerDocument : NSPersistentDocument
+@interface StackTimerDocument : NSPersistentDocument <NSApplicationDelegate, NSSpeechSynthesizerDelegate>
+{
+    NSSpeechSynthesizer *_speechSynth;
+    NSStatusItem* statusItem;
+}
+@property (strong) IBOutlet NSPopover *formPopover;
+@property (strong) IBOutlet NSMenu *detailMenu;
+@property (weak) IBOutlet NSTextFieldCell *textField;
+@property (weak) IBOutlet NSTextField *input;
+@property (weak) IBOutlet NSTextField *output;
+
+- (IBAction)seed:(id)sender;
+- (IBAction)generate:(id)sender;
+- (IBAction)speak:(id)sender;
+- (IBAction)stopSpeak:(id)sender;
+- (IBAction)countInput:(id)sender;
+- (IBAction)showMenulet:(id)sender;
+
 
 @end
--- StackTimer/StackTimerDocument.m
+++ StackTimer/StackTimerDocument.m
@@ -9,14 +9,33 @@
 #import "StackTimerDocument.h"
 
 @implementation StackTimerDocument
-
+@synthesize detailMenu;
+@synthesize formPopover;
+@synthesize textField;
+@synthesize input;
+@synthesize output;
 - (id)init
 {
     self = [super init];
     if (self) {
         // Add your subclass-specific initialization here.
+        _speechSynth = [[NSSpeechSynthesizer alloc]
+                        initWithVoice:nil];
+        [_speechSynth setDelegate:self];
     }
     return self;
+}
+- (void)awakeFromNib
+{
+    NSDate *now;
+    now = [NSDate date];
+    [textField setObjectValue:now];
+    
+    CGFloat thickness = [[NSStatusBar systemStatusBar] thickness];
+    statusItem = [[NSStatusBar systemStatusBar] statusItemWithLength:thickness];
+    [statusItem setMenu:detailMenu];
+    [statusItem setTitle:@"Status"];
+    [statusItem setHighlightMode:YES];
 }
 
 - (NSString *)windowNibName
@@ -36,5 +55,51 @@
 {
     return YES;
 }
+- (void)speechSynthesizer:(NSSpeechSynthesizer *)sender
+        didFinishSpeaking:(BOOL)finishedSpeaking
+{
+    NSLog(@"finishedSpeaking = %d", finishedSpeaking);
+}
 
+- (IBAction)seed:(id)sender {
+    // Generate a number between 1 and 100 inclusive
+    int generated;
+    generated = (int)(random() % 100) + 1;
+    NSLog(@"generated = %d", generated);
+    // Ask the text field to change what it is displaying
+    [textField setIntValue:generated];
+}
+
+- (IBAction)generate:(id)sender {
+    // Seed the random number generator with the time
+    srandom((unsigned)time(NULL));
+    [textField setStringValue:@"Generator seeded"];
+}
+
+- (IBAction)speak:(id)sender {
+    NSString *string = [input stringValue];
+    // Is the string zero-length?
+    if ([string length] == 0) {
+        NSLog(@"string from %@ is of zero-length", textField);
+        return; }
+    [_speechSynth startSpeakingString:string];
+    NSLog(@"Have started to say: %@", string);
+}
+
+- (IBAction)stopSpeak:(id)sender {
+    [_speechSynth stopSpeaking];
+}
+
+- (IBAction)countInput:(id)sender {
+    NSString *string = [input stringValue];
+    if ([string length] == 0) {
+        [output setStringValue:@"???"];
+    } else {
+        [output setStringValue:[NSString stringWithFormat:@"'%@'has %d char", string, [string length]]];
+    }
+}
+
+- (IBAction)showMenulet:(id)sender {
+    [formPopover showRelativeToRect:[sender bounds] ofView:sender preferredEdge:NSMaxYEdge];
+}
 @end
