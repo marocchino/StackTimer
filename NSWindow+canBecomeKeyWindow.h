5d9a9bbf6446e223e80bce815726133752654766
tw shim <marocchino@gmail.com>
2012-06-07 03:04:32 +0900

    add hot fix via http://stackoverflow.com/questions/7214273/nstextfield-on-nspopover

---
+++ NSWindow+canBecomeKeyWindow.h
@@ -0,0 +1,13 @@
+//
+//  NSWindow+canBecomeKeyWindow.h
+//  StackTimer
+//
+//  Created by Taewon Shim on 6/7/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface NSWindow (canBecomeKeyWindow)
+
+@end
