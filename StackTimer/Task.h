f51cc7d49f519660c03c76c93c0c8500f83be2ef
tw shim <marocchino@gmail.com>
2012-06-07 07:43:30 +0900

    rename model, add depth column

---
+++ StackTimer/Task.h
@@ -0,0 +1,23 @@
+//
+//  Task.h
+//  StackTimer
+//
+//  Created by Taewon Shim on 6/7/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreData/CoreData.h>
+
+@class Task;
+
+@interface Task : NSManagedObject
+
+@property (nonatomic, retain) NSString * title;
+@property (nonatomic, retain) NSDate * startedAt;
+@property (nonatomic, retain) NSDate * endedAt;
+@property (nonatomic, retain) NSNumber * status;
+@property (nonatomic, retain) NSNumber * depth;
+@property (nonatomic, retain) Task *parent;
+
+@end
---
+++ StackTimer/Task.m
@@ -0,0 +1,22 @@
+//
+//  Task.m
+//  StackTimer
+//
+//  Created by Taewon Shim on 6/7/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "Task.h"
+#import "Task.h"
+
+
+@implementation Task
+
+@dynamic title;
+@dynamic startedAt;
+@dynamic endedAt;
+@dynamic status;
+@dynamic depth;
+@dynamic parent;
+
+@end
