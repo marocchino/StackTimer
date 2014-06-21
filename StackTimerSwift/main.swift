0aa446717828d2da605f608db4e221e0cb175aff
Shim Tw <marocchino@gmail.com>
2014-06-21 12:25:27 +0900

    Initial Commit

---
+++ StackTimerSwift/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  StackTimerSwift
+//
+//  Created by Shim Tw on 6/21/14.
+//  Copyright (c) 2014 Tw Shim. All rights reserved.
+//
+
+import Cocoa
+
+NSApplicationMain(C_ARGC, C_ARGV)
